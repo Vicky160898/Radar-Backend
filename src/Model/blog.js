c8b4e20e57146e455aabdbbcf0d9ccdb36bab774
vinayak hiwase <vinayakhiwse8@gmail.com>
2024-03-25 11:51:53 +0530

    backend-fixed

--- src/Model/blog.js
+++ src/Model/blog.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require("mongoose");
 
 const BlogModel = new Schema({
-  title: { type: String, required: true, unique: true },
-  desc: { type: String, required: true, unique: true },
+  title: { type: String, required: true},
+  desc: { type: String, required: true },
   category: { type: String, required: true },
   author_ID: { type: Schema.Types.ObjectId, ref: "Author", required: true },
-  category_desc: { type: String, required: true, unique: true },
+  category_desc: { type: String, required: true },
   createdAt: { type: Date, default: Date.now() },
   image: { type: String }, 
 });
